b2f3226d7706a47eea533cff743cd283e02324b4
Nick Nassiri <nick206@gmail.com>
2026-08-06 18:38:36 -0700

    test262: pin Math log2 descriptor parity

--- SharpTS.Test262/Issue1279ParityTests.cs
+++ SharpTS.Test262/Issue1279ParityTests.cs
@@ -1316,6 +1316,7 @@ public sealed class Issue1279ParityTests
     [InlineData("built-ins/Math/log/prop-desc.js")]
     [InlineData("built-ins/Math/log10/prop-desc.js")]
     [InlineData("built-ins/Math/log1p/prop-desc.js")]
+    [InlineData("built-ins/Math/log2/prop-desc.js")]
     public void Math_members_have_standard_descriptors(string relativePath)
         => AssertPassInBothModes(relativePath);
 
