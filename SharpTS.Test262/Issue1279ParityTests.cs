60ebfa71edd03b74e09c9358f262e937b54d80d6
Nick Nassiri <nick206@gmail.com>
2026-08-07 01:15:29 -0700

    test(interp): cover endsWith RegExp rejection

--- SharpTS.Test262/Issue1279ParityTests.cs
+++ SharpTS.Test262/Issue1279ParityTests.cs
@@ -2199,6 +2199,11 @@ public sealed class Issue1279ParityTests
         => AssertPass(relativePath, Test262ExecutionMode.Interpreted);
 
     [Theory]
+    [InlineData("built-ins/String/prototype/endsWith/searchstring-is-regexp-throws.js")]
+    public void String_endsWith_rejects_regexp_search_strings(string relativePath)
+        => AssertPass(relativePath, Test262ExecutionMode.Interpreted);
+
+    [Theory]
     [InlineData("built-ins/Array/prototype/flat/array-like-objects.js")]
     [InlineData("built-ins/Array/prototype/flatMap/array-like-objects-nested.js")]
     [InlineData("built-ins/Array/prototype/flatMap/array-like-objects-poisoned-length.js")]
