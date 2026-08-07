787ee2790a807cfa41c791b522a115d9d9f82bda
Nick Nassiri <nick206@gmail.com>
2026-08-06 18:38:08 -0700

    test262: pin Math imul descriptor parity

--- SharpTS.Test262/Issue1279ParityTests.cs
+++ SharpTS.Test262/Issue1279ParityTests.cs
@@ -1312,6 +1312,7 @@ public sealed class Issue1279ParityTests
     [InlineData("built-ins/Math/floor/prop-desc.js")]
     [InlineData("built-ins/Math/fround/prop-desc.js")]
     [InlineData("built-ins/Math/hypot/prop-desc.js")]
+    [InlineData("built-ins/Math/imul/prop-desc.js")]
     public void Math_members_have_standard_descriptors(string relativePath)
         => AssertPassInBothModes(relativePath);
 
