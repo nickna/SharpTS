4c30ed31a481417b7f06fc6b68d88baf069ce61d
Nick Nassiri <nick206@gmail.com>
2026-08-06 18:39:02 -0700

    test262: pin Math min descriptor parity

--- SharpTS.Test262/Issue1279ParityTests.cs
+++ SharpTS.Test262/Issue1279ParityTests.cs
@@ -1318,6 +1318,7 @@ public sealed class Issue1279ParityTests
     [InlineData("built-ins/Math/log1p/prop-desc.js")]
     [InlineData("built-ins/Math/log2/prop-desc.js")]
     [InlineData("built-ins/Math/max/prop-desc.js")]
+    [InlineData("built-ins/Math/min/prop-desc.js")]
     public void Math_members_have_standard_descriptors(string relativePath)
         => AssertPassInBothModes(relativePath);
 
