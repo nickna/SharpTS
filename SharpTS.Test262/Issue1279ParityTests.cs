ca8074018362400cb23788a7250785614507cf9f
Nick Nassiri <nick206@gmail.com>
2026-08-06 18:37:06 -0700

    test262: pin Math cos descriptor parity

--- SharpTS.Test262/Issue1279ParityTests.cs
+++ SharpTS.Test262/Issue1279ParityTests.cs
@@ -1305,6 +1305,7 @@ public sealed class Issue1279ParityTests
     [InlineData("built-ins/Math/cbrt/prop-desc.js")]
     [InlineData("built-ins/Math/ceil/prop-desc.js")]
     [InlineData("built-ins/Math/clz32/prop-desc.js")]
+    [InlineData("built-ins/Math/cos/prop-desc.js")]
     public void Math_members_have_standard_descriptors(string relativePath)
         => AssertPassInBothModes(relativePath);
 
