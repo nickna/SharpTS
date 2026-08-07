3e85fb4c336c385676a18b1029eb8405ef20a59b
Nick Nassiri <nick206@gmail.com>
2026-08-06 18:37:48 -0700

    test262: pin Math floor descriptor parity

--- SharpTS.Test262/Issue1279ParityTests.cs
+++ SharpTS.Test262/Issue1279ParityTests.cs
@@ -1309,6 +1309,7 @@ public sealed class Issue1279ParityTests
     [InlineData("built-ins/Math/cosh/prop-desc.js")]
     [InlineData("built-ins/Math/exp/prop-desc.js")]
     [InlineData("built-ins/Math/expm1/prop-desc.js")]
+    [InlineData("built-ins/Math/floor/prop-desc.js")]
     public void Math_members_have_standard_descriptors(string relativePath)
         => AssertPassInBothModes(relativePath);
 
