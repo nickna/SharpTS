fa560bf4d8ea4dae30078f35b92fd59e29bc308d
Nick Nassiri <nick206@gmail.com>
2026-08-07 01:14:52 -0700

    test(interp): cover startsWith Symbol match

--- SharpTS.Tests/SharedTests/StringMethodTests.cs
+++ SharpTS.Tests/SharedTests/StringMethodTests.cs
@@ -9,6 +9,26 @@ namespace SharpTS.Tests.SharedTests;
 public class StringMethodTests
 {
     [Fact]
+    public void String_StartsWith_UsesSymbolMatchForRegExpDetection_InInterpreter()
+    {
+        var output = TestHarness.RunInterpreted("""
+            const disabled: any = /x/;
+            disabled[Symbol.match] = false;
+            console.log("/x/value".startsWith(disabled));
+
+            const enabled: any = { toString: function(): string { return "x"; } };
+            enabled[Symbol.match] = true;
+            try {
+                "xyz".startsWith(enabled);
+            } catch (error) {
+                console.log("threw");
+            }
+            """);
+
+        Assert.Equal("true\nthrew\n", output);
+    }
+
+    [Fact]
     public void String_Includes_UsesSymbolMatchForRegExpDetection_InInterpreter()
     {
         var output = TestHarness.RunInterpreted("""
