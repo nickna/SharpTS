d81af995ba202f78b2c1a8906434892021a58642
Nick Nassiri <nick206@gmail.com>
2026-08-06 18:39:58 -0700

    test262: pin Math tan descriptor parity

--- SharpTS.Test262/Issue1279ParityTests.cs
+++ SharpTS.Test262/Issue1279ParityTests.cs
@@ -1326,6 +1326,7 @@ public sealed class Issue1279ParityTests
     [InlineData("built-ins/Math/sin/prop-desc.js")]
     [InlineData("built-ins/Math/sinh/prop-desc.js")]
     [InlineData("built-ins/Math/sqrt/prop-desc.js")]
+    [InlineData("built-ins/Math/tan/prop-desc.js")]
     public void Math_members_have_standard_descriptors(string relativePath)
         => AssertPassInBothModes(relativePath);
 
