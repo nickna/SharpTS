1befc6863d2c048ac7a20cbd34ab91aaee163b23
Nick Nassiri <nick206@gmail.com>
2026-08-07 21:36:59 -0700

    test(interp): guard Array.from iterable element creation

--- SharpTS.Test262/Issue1279ParityTests.cs
+++ SharpTS.Test262/Issue1279ParityTests.cs
@@ -3045,6 +3045,7 @@ public sealed class Issue1279ParityTests
     [InlineData("built-ins/Array/from/iter-map-fn-this-non-strict.js")]
     [InlineData("built-ins/Array/from/iter-map-fn-this-strict.js")]
     [InlineData("built-ins/Array/from/iter-set-elem-prop-non-writable.js")]
+    [InlineData("built-ins/Array/from/iter-set-elem-prop.js")]
     public void Array_from_preserves_iterable_and_array_like_semantics(string relativePath)
         => AssertPass(relativePath, Test262ExecutionMode.Interpreted);
 
