a5edbc481cd5e0a3d47d301737ce19e13c95cea0
Nick Nassiri <nick206@gmail.com>
2026-08-08 01:32:20 -0700

    test(interp): guard fill symbol end errors

--- SharpTS.Test262/Issue1279ParityTests.cs
+++ SharpTS.Test262/Issue1279ParityTests.cs
@@ -3190,6 +3190,7 @@ public sealed class Issue1279ParityTests
     [InlineData("built-ins/Array/prototype/fill/coerced-indexes.js")]
     [InlineData("built-ins/Array/prototype/fill/fill-values.js")]
     [InlineData("built-ins/Array/prototype/fill/length-near-integer-limit.js")]
+    [InlineData("built-ins/Array/prototype/fill/return-abrupt-from-end-as-symbol.js")]
     public void Array_fill_mutates_generic_receivers(string relativePath)
         => AssertPass(relativePath, Test262ExecutionMode.Interpreted);
 
