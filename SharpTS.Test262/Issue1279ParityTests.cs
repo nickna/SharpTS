1c63ef2b25eddd78608ca9571a796435935aef80
Nick Nassiri <nick206@gmail.com>
2026-08-07 21:31:24 -0700

    test(interp): guard Array.from Array source

--- SharpTS.Test262/Issue1279ParityTests.cs
+++ SharpTS.Test262/Issue1279ParityTests.cs
@@ -3032,6 +3032,7 @@ public sealed class Issue1279ParityTests
     [InlineData("built-ins/Array/from/calling-from-valid-1-onlyStrict.js")]
     [InlineData("built-ins/Array/from/elements-added-after.js")]
     [InlineData("built-ins/Array/from/elements-updated-after.js")]
+    [InlineData("built-ins/Array/from/from-array.js")]
     public void Array_from_preserves_iterable_and_array_like_semantics(string relativePath)
         => AssertPass(relativePath, Test262ExecutionMode.Interpreted);
 
