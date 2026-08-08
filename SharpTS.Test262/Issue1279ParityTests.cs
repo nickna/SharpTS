5ffe0af5f11b04957a3a25b97c9521f2e72392da
Nick Nassiri <nick206@gmail.com>
2026-08-08 01:24:43 -0700

    test(interp): guard unshift frozen array length

--- SharpTS.Test262/Issue1279ParityTests.cs
+++ SharpTS.Test262/Issue1279ParityTests.cs
@@ -3165,6 +3165,7 @@ public sealed class Issue1279ParityTests
     [InlineData("built-ins/Array/prototype/unshift/S15.4.4.13_A4_T1.js")]
     [InlineData("built-ins/Array/prototype/unshift/S15.4.4.13_A4_T2.js")]
     [InlineData("built-ins/Array/prototype/unshift/length-near-integer-limit.js")]
+    [InlineData("built-ins/Array/prototype/unshift/set-length-array-is-frozen.js")]
     public void Array_unshift_mutates_generic_receivers(string relativePath)
         => AssertPass(relativePath, Test262ExecutionMode.Interpreted);
 
