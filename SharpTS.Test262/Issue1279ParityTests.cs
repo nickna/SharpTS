6d7c57898e506dad53a3e8ac951ab1e4105dfdef
Nick Nassiri <nick206@gmail.com>
2026-08-08 01:15:26 -0700

    test(interp): guard generic push numeric lengths

--- SharpTS.Test262/Issue1279ParityTests.cs
+++ SharpTS.Test262/Issue1279ParityTests.cs
@@ -3126,6 +3126,7 @@ public sealed class Issue1279ParityTests
 
     [Theory]
     [InlineData("built-ins/Array/prototype/push/S15.4.4.7_A2_T1.js")]
+    [InlineData("built-ins/Array/prototype/push/S15.4.4.7_A2_T2.js")]
     public void Array_push_mutates_generic_receivers(string relativePath)
         => AssertPass(relativePath, Test262ExecutionMode.Interpreted);
 
