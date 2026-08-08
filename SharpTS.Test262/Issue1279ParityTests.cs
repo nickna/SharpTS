2c7b99e613816a411bcb2f293b7525115198377e
Nick Nassiri <nick206@gmail.com>
2026-08-08 01:08:22 -0700

    test(interp): guard pop boolean receivers

--- SharpTS.Test262/Issue1279ParityTests.cs
+++ SharpTS.Test262/Issue1279ParityTests.cs
@@ -3114,6 +3114,7 @@ public sealed class Issue1279ParityTests
     [InlineData("built-ins/Array/prototype/pop/S15.4.4.6_A3_T3.js")]
     [InlineData("built-ins/Array/prototype/pop/S15.4.4.6_A4_T1.js")]
     [InlineData("built-ins/Array/prototype/pop/S15.4.4.6_A4_T2.js")]
+    [InlineData("built-ins/Array/prototype/pop/call-with-boolean.js")]
     public void Array_pop_mutates_generic_receivers(string relativePath)
         => AssertPass(relativePath, Test262ExecutionMode.Interpreted);
 
