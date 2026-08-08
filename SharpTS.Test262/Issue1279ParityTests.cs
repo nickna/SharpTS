f2b1e1b677c91fbe9550fd760942af08efc34b6e
Nick Nassiri <nick206@gmail.com>
2026-08-08 01:19:04 -0700

    test(interp): guard generic shift missing lengths

--- SharpTS.Test262/Issue1279ParityTests.cs
+++ SharpTS.Test262/Issue1279ParityTests.cs
@@ -3141,6 +3141,11 @@ public sealed class Issue1279ParityTests
     public void Array_push_mutates_generic_receivers(string relativePath)
         => AssertPass(relativePath, Test262ExecutionMode.Interpreted);
 
+    [Theory]
+    [InlineData("built-ins/Array/prototype/shift/S15.4.4.9_A2_T1.js")]
+    public void Array_shift_mutates_generic_receivers(string relativePath)
+        => AssertPass(relativePath, Test262ExecutionMode.Interpreted);
+
     private void AssertPass(string relativePath, Test262ExecutionMode mode)
     {
         var root = Test262Paths.TryFindRoot();
