c78867a3187ed640db61824264f53420b8765d38
Nick Nassiri <nick206@gmail.com>
2026-08-08 00:46:12 -0700

    test(interp): guard concat spreadable String wrappers

--- SharpTS.Test262/Issue1279ParityTests.cs
+++ SharpTS.Test262/Issue1279ParityTests.cs
@@ -3081,6 +3081,7 @@ public sealed class Issue1279ParityTests
     [InlineData("built-ins/Array/prototype/concat/is-concat-spreadable-get-err.js")]
     [InlineData("built-ins/Array/prototype/concat/is-concat-spreadable-val-falsey.js")]
     [InlineData("built-ins/Array/prototype/concat/is-concat-spreadable-val-truthy.js")]
+    [InlineData("built-ins/Array/prototype/concat/Array.prototype.concat_spreadable-string-wrapper.js")]
     [InlineData("built-ins/Array/prototype/concat/Array.prototype.concat_spreadable-number-wrapper.js")]
     [InlineData("built-ins/Array/prototype/concat/Array.prototype.concat_spreadable-function.js")]
     [InlineData("built-ins/Array/prototype/concat/Array.prototype.concat_spreadable-boolean-wrapper.js")]
