6de3d455c7847c9a0b61f41b3e999c0b0fa9a8b0
Nick Nassiri <nick206@gmail.com>
2026-08-08 01:07:36 -0700

    test(interp): guard generic pop numeric lengths

--- SharpTS.Test262/Issue1279ParityTests.cs
+++ SharpTS.Test262/Issue1279ParityTests.cs
@@ -3106,6 +3106,7 @@ public sealed class Issue1279ParityTests
 
     [Theory]
     [InlineData("built-ins/Array/prototype/pop/S15.4.4.6_A2_T1.js")]
+    [InlineData("built-ins/Array/prototype/pop/S15.4.4.6_A2_T2.js")]
     public void Array_pop_mutates_generic_receivers(string relativePath)
         => AssertPass(relativePath, Test262ExecutionMode.Interpreted);
 
