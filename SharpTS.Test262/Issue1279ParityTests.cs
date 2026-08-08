b3c1e8e4ebe92026afddebb1a890efcdbb22b502
Nick Nassiri <nick206@gmail.com>
2026-08-08 01:27:42 -0700

    test(interp): guard generic reverse missing lengths

--- SharpTS.Test262/Issue1279ParityTests.cs
+++ SharpTS.Test262/Issue1279ParityTests.cs
@@ -3172,6 +3172,11 @@ public sealed class Issue1279ParityTests
     public void Array_unshift_mutates_generic_receivers(string relativePath)
         => AssertPass(relativePath, Test262ExecutionMode.Interpreted);
 
+    [Theory]
+    [InlineData("built-ins/Array/prototype/reverse/S15.4.4.8_A2_T1.js")]
+    public void Array_reverse_mutates_generic_receivers(string relativePath)
+        => AssertPass(relativePath, Test262ExecutionMode.Interpreted);
+
     private void AssertPass(string relativePath, Test262ExecutionMode mode)
     {
         var root = Test262Paths.TryFindRoot();
