d71eed7810fc8c8dbec76ea39fcfb701176aa009
Nick Nassiri <nick206@gmail.com>
2026-08-08 01:08:47 -0700

    test(interp): guard empty readonly array pop

--- SharpTS.Test262/Issue1279ParityTests.cs
+++ SharpTS.Test262/Issue1279ParityTests.cs
@@ -3120,6 +3120,7 @@ public sealed class Issue1279ParityTests
     [InlineData("built-ins/Array/prototype/pop/set-length-array-is-frozen.js")]
     [InlineData("built-ins/Array/prototype/pop/set-length-array-length-is-non-writable.js")]
     [InlineData("built-ins/Array/prototype/pop/set-length-zero-array-is-frozen.js")]
+    [InlineData("built-ins/Array/prototype/pop/set-length-zero-array-length-is-non-writable.js")]
     public void Array_pop_mutates_generic_receivers(string relativePath)
         => AssertPass(relativePath, Test262ExecutionMode.Interpreted);
 
