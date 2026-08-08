be8fa6d5aafe8704a6c089a977dcd4c37c5c32de
Nick Nassiri <nick206@gmail.com>
2026-08-08 01:15:38 -0700

    test(interp): guard generic push at uint range

--- SharpTS.Test262/Issue1279ParityTests.cs
+++ SharpTS.Test262/Issue1279ParityTests.cs
@@ -3129,6 +3129,7 @@ public sealed class Issue1279ParityTests
     [InlineData("built-ins/Array/prototype/push/S15.4.4.7_A2_T2.js")]
     [InlineData("built-ins/Array/prototype/push/S15.4.4.7_A3.js")]
     [InlineData("built-ins/Array/prototype/push/S15.4.4.7_A4_T1.js")]
+    [InlineData("built-ins/Array/prototype/push/S15.4.4.7_A4_T2.js")]
     public void Array_push_mutates_generic_receivers(string relativePath)
         => AssertPass(relativePath, Test262ExecutionMode.Interpreted);
 
