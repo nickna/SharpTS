a599e50b161af0a20d21a8fe832cded69866874f
Nick Nassiri <nick206@gmail.com>
2026-08-07 21:36:01 -0700

    test(interp): guard Array.from strict mapping this

--- SharpTS.Test262/Issue1279ParityTests.cs
+++ SharpTS.Test262/Issue1279ParityTests.cs
@@ -3043,6 +3043,7 @@ public sealed class Issue1279ParityTests
     [InlineData("built-ins/Array/from/iter-map-fn-err.js")]
     [InlineData("built-ins/Array/from/iter-map-fn-return.js")]
     [InlineData("built-ins/Array/from/iter-map-fn-this-non-strict.js")]
+    [InlineData("built-ins/Array/from/iter-map-fn-this-strict.js")]
     public void Array_from_preserves_iterable_and_array_like_semantics(string relativePath)
         => AssertPass(relativePath, Test262ExecutionMode.Interpreted);
 
