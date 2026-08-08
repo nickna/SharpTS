d7c9a73835476f77b377829ee8591d6638c26ca9
Nick Nassiri <nick206@gmail.com>
2026-08-07 21:42:43 -0700

    test(interp): guard Array.isArray function identity

--- SharpTS.Test262/Issue1279ParityTests.cs
+++ SharpTS.Test262/Issue1279ParityTests.cs
@@ -3062,6 +3062,7 @@ public sealed class Issue1279ParityTests
 
     [Theory]
     [InlineData("built-ins/Array/isArray/15.4.3.2-0-1.js")]
+    [InlineData("built-ins/Array/isArray/15.4.3.2-0-2.js")]
     public void Array_isArray_preserves_cross_type_classification(string relativePath)
         => AssertPass(relativePath, Test262ExecutionMode.Interpreted);
 
