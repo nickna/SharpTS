1734c08deed75831ca2b151d4317d85c5a25d1db
Nick Nassiri <nick206@gmail.com>
2026-08-07 21:40:14 -0700

    test(interp): guard Array.from source length coercion

--- SharpTS.Test262/Issue1279ParityTests.cs
+++ SharpTS.Test262/Issue1279ParityTests.cs
@@ -3053,6 +3053,7 @@ public sealed class Issue1279ParityTests
     [InlineData("built-ins/Array/from/source-object-iterator-1.js")]
     [InlineData("built-ins/Array/from/source-object-iterator-2.js")]
     [InlineData("built-ins/Array/from/source-object-length-set-elem-prop-non-writable.js")]
+    [InlineData("built-ins/Array/from/source-object-length.js")]
     public void Array_from_preserves_iterable_and_array_like_semantics(string relativePath)
         => AssertPass(relativePath, Test262ExecutionMode.Interpreted);
 
