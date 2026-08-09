e363141119083b5442cfc8e50d2737df48401b5f
Nick Nassiri <nick206@gmail.com>
2026-08-09 07:04:38 -0700

    test(interp): cover Proxy revocable length

--- SharpTS.Test262/Issue1279ParityTests.cs
+++ SharpTS.Test262/Issue1279ParityTests.cs
@@ -2694,6 +2694,12 @@ public sealed class Issue1279ParityTests
             "built-ins/Proxy/revocable/handler-is-revoked-proxy.js",
             Test262ExecutionMode.Interpreted);
 
+    [Fact]
+    public void Proxy_revocable_has_standard_length()
+        => AssertPass(
+            "built-ins/Proxy/revocable/length.js",
+            Test262ExecutionMode.Interpreted);
+
     [Theory]
     [InlineData("built-ins/Object/defineProperty/15.2.3.6-4-623.js")]
     [InlineData("built-ins/Object/defineProperty/15.2.3.6-4-624.js")]
