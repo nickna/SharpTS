861c7fc02f6739c645a073b6efee10f493a2f27a
Nick Nassiri <nick206@gmail.com>
2026-08-09 05:19:02 -0700

    test(interp): accept matching fixed prototypes

--- SharpTS.Test262/Issue1279ParityTests.cs
+++ SharpTS.Test262/Issue1279ParityTests.cs
@@ -1992,6 +1992,12 @@ public sealed class Issue1279ParityTests
             "built-ins/Proxy/setPrototypeOf/not-extensible-target-not-same-target-prototype.js",
             Test262ExecutionMode.Interpreted);
 
+    [Fact]
+    public void Proxy_setPrototypeOf_accepts_matching_fixed_target_prototypes()
+        => AssertPass(
+            "built-ins/Proxy/setPrototypeOf/not-extensible-target-same-target-prototype.js",
+            Test262ExecutionMode.Interpreted);
+
     [Theory]
     [InlineData("built-ins/Object/defineProperty/15.2.3.6-4-623.js")]
     [InlineData("built-ins/Object/defineProperty/15.2.3.6-4-624.js")]
