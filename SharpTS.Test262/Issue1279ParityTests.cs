b0bb7d2ad3dc8af08cdb7d2910987b0c473c39c5
Nick Nassiri <nick206@gmail.com>
2026-08-09 03:58:30 -0700

    test(interp): cover invalid proxy ownKeys traps

--- SharpTS.Test262/Issue1279ParityTests.cs
+++ SharpTS.Test262/Issue1279ParityTests.cs
@@ -1561,6 +1561,12 @@ public sealed class Issue1279ParityTests
             "built-ins/Proxy/ownKeys/null-handler.js",
             Test262ExecutionMode.Interpreted);
 
+    [Fact]
+    public void Proxy_ownKeys_rejects_noncallable_traps()
+        => AssertPass(
+            "built-ins/Proxy/ownKeys/trap-is-not-callable.js",
+            Test262ExecutionMode.Interpreted);
+
     [Theory]
     [InlineData("built-ins/Object/defineProperty/15.2.3.6-4-623.js")]
     [InlineData("built-ins/Object/defineProperty/15.2.3.6-4-624.js")]
