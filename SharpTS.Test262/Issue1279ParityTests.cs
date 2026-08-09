78134ff2c42f88541cc473cb4e442cfcfba7594a
Nick Nassiri <nick206@gmail.com>
2026-08-09 06:05:09 -0700

    test(interp): accept matching fixed proxy gets

--- SharpTS.Test262/Issue1279ParityTests.cs
+++ SharpTS.Test262/Issue1279ParityTests.cs
@@ -2316,6 +2316,12 @@ public sealed class Issue1279ParityTests
             "built-ins/Proxy/get/return-trap-result-configurable-true-writable-false.js",
             Test262ExecutionMode.Interpreted);
 
+    [Fact]
+    public void Proxy_get_accepts_same_value_for_fixed_data_properties()
+        => AssertPass(
+            "built-ins/Proxy/get/return-trap-result-same-value-configurable-false-writable-false.js",
+            Test262ExecutionMode.Interpreted);
+
     [Theory]
     [InlineData("built-ins/Object/defineProperty/15.2.3.6-4-623.js")]
     [InlineData("built-ins/Object/defineProperty/15.2.3.6-4-624.js")]
