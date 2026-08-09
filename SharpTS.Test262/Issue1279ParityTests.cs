a3da77053dee224bf2e704d156995eb996cff49e
Nick Nassiri <nick206@gmail.com>
2026-08-09 04:34:18 -0700

    test(interp): cover abrupt descriptor traps

--- SharpTS.Test262/Issue1279ParityTests.cs
+++ SharpTS.Test262/Issue1279ParityTests.cs
@@ -1680,6 +1680,12 @@ public sealed class Issue1279ParityTests
             "built-ins/Proxy/getOwnPropertyDescriptor/trap-is-not-callable.js",
             Test262ExecutionMode.Interpreted);
 
+    [Fact]
+    public void Proxy_getOwnPropertyDescriptor_propagates_abrupt_trap_completion()
+        => AssertPass(
+            "built-ins/Proxy/getOwnPropertyDescriptor/return-is-abrupt.js",
+            Test262ExecutionMode.Interpreted);
+
     [Theory]
     [InlineData("built-ins/Object/defineProperty/15.2.3.6-4-623.js")]
     [InlineData("built-ins/Object/defineProperty/15.2.3.6-4-624.js")]
