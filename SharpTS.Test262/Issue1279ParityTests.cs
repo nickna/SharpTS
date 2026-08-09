5b7bc0e972dde1728df37b959cfc10d26e517884
Nick Nassiri <nick206@gmail.com>
2026-08-09 06:12:33 -0700

    test(interp): return undefined for absent proxy gets

--- SharpTS.Test262/Issue1279ParityTests.cs
+++ SharpTS.Test262/Issue1279ParityTests.cs
@@ -2346,6 +2346,12 @@ public sealed class Issue1279ParityTests
             "built-ins/Proxy/get/trap-is-null-target-is-proxy.js",
             Test262ExecutionMode.Interpreted);
 
+    [Fact]
+    public void Proxy_get_returns_undefined_for_absent_forwarded_properties()
+        => AssertPass(
+            "built-ins/Proxy/get/trap-is-undefined-no-property.js",
+            Test262ExecutionMode.Interpreted);
+
     [Theory]
     [InlineData("built-ins/Object/defineProperty/15.2.3.6-4-623.js")]
     [InlineData("built-ins/Object/defineProperty/15.2.3.6-4-624.js")]
