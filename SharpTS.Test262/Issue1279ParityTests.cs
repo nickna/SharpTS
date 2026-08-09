71e85881488752c0c6dab3be2e4de7ac2ec45175
Nick Nassiri <nick206@gmail.com>
2026-08-09 07:07:17 -0700

    test(interp): return proxies from Proxy revocable

--- SharpTS.Test262/Issue1279ParityTests.cs
+++ SharpTS.Test262/Issue1279ParityTests.cs
@@ -2712,6 +2712,12 @@ public sealed class Issue1279ParityTests
             "built-ins/Proxy/revocable/not-a-constructor.js",
             Test262ExecutionMode.Interpreted);
 
+    [Fact]
+    public void Proxy_revocable_returns_a_proxy()
+        => AssertPass(
+            "built-ins/Proxy/revocable/proxy.js",
+            Test262ExecutionMode.Interpreted);
+
     [Theory]
     [InlineData("built-ins/Object/defineProperty/15.2.3.6-4-623.js")]
     [InlineData("built-ins/Object/defineProperty/15.2.3.6-4-624.js")]
