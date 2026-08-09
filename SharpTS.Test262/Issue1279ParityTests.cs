f8ca3ebadd5f5e263d4efb71c49084ef534c0fa7
Nick Nassiri <nick206@gmail.com>
2026-08-09 06:46:40 -0700

    test(interp): forward null proxy apply traps

--- SharpTS.Test262/Issue1279ParityTests.cs
+++ SharpTS.Test262/Issue1279ParityTests.cs
@@ -2562,6 +2562,12 @@ public sealed class Issue1279ParityTests
             "built-ins/Proxy/apply/trap-is-not-callable.js",
             Test262ExecutionMode.Interpreted);
 
+    [Fact]
+    public void Proxy_apply_forwards_null_traps()
+        => AssertPass(
+            "built-ins/Proxy/apply/trap-is-null.js",
+            Test262ExecutionMode.Interpreted);
+
     [Theory]
     [InlineData("built-ins/Object/defineProperty/15.2.3.6-4-623.js")]
     [InlineData("built-ins/Object/defineProperty/15.2.3.6-4-624.js")]
