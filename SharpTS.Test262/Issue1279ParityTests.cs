0b3afb3d71307d6b6e8f02f7a0bdc7413f67326d
Nick Nassiri <nick206@gmail.com>
2026-08-09 06:05:46 -0700

    test(interp): return proxy get trap results

--- SharpTS.Test262/Issue1279ParityTests.cs
+++ SharpTS.Test262/Issue1279ParityTests.cs
@@ -2322,6 +2322,12 @@ public sealed class Issue1279ParityTests
             "built-ins/Proxy/get/return-trap-result-same-value-configurable-false-writable-false.js",
             Test262ExecutionMode.Interpreted);
 
+    [Fact]
+    public void Proxy_get_returns_trap_results()
+        => AssertPass(
+            "built-ins/Proxy/get/return-trap-result.js",
+            Test262ExecutionMode.Interpreted);
+
     [Theory]
     [InlineData("built-ins/Object/defineProperty/15.2.3.6-4-623.js")]
     [InlineData("built-ins/Object/defineProperty/15.2.3.6-4-624.js")]
