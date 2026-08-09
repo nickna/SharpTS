599cbb50151e0c05d94a95d42734181324a2b999
Nick Nassiri <nick206@gmail.com>
2026-08-09 07:13:26 -0700

    test(interp): return undefined from Proxy revokers

--- SharpTS.Test262/Issue1279ParityTests.cs
+++ SharpTS.Test262/Issue1279ParityTests.cs
@@ -2754,6 +2754,12 @@ public sealed class Issue1279ParityTests
             "built-ins/Proxy/revocable/revocation-function-property-order.js",
             Test262ExecutionMode.Interpreted);
 
+    [Fact]
+    public void Proxy_revocation_returns_undefined()
+        => AssertPass(
+            "built-ins/Proxy/revocable/revoke-returns-undefined.js",
+            Test262ExecutionMode.Interpreted);
+
     [Theory]
     [InlineData("built-ins/Object/defineProperty/15.2.3.6-4-623.js")]
     [InlineData("built-ins/Object/defineProperty/15.2.3.6-4-624.js")]
