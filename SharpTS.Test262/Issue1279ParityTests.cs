80cf620292934da1f57c39790fc71130117266ee
Nick Nassiri <nick206@gmail.com>
2026-08-09 07:20:02 -0700

    test(interp): reject null Proxy handlers

--- SharpTS.Test262/Issue1279ParityTests.cs
+++ SharpTS.Test262/Issue1279ParityTests.cs
@@ -2796,6 +2796,12 @@ public sealed class Issue1279ParityTests
             "built-ins/Proxy/create-handler-not-object-throw-boolean.js",
             Test262ExecutionMode.Interpreted);
 
+    [Fact]
+    public void Proxy_constructor_rejects_null_handlers()
+        => AssertPass(
+            "built-ins/Proxy/create-handler-not-object-throw-null.js",
+            Test262ExecutionMode.Interpreted);
+
     [Theory]
     [InlineData("built-ins/Object/defineProperty/15.2.3.6-4-623.js")]
     [InlineData("built-ins/Object/defineProperty/15.2.3.6-4-624.js")]
