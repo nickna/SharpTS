88b400aa114add1302b3be2167c23e2e6c668238
Nick Nassiri <nick206@gmail.com>
2026-08-09 07:24:19 -0700

    test(interp): reject null Proxy targets

--- SharpTS.Test262/Issue1279ParityTests.cs
+++ SharpTS.Test262/Issue1279ParityTests.cs
@@ -2832,6 +2832,12 @@ public sealed class Issue1279ParityTests
             "built-ins/Proxy/create-target-not-object-throw-boolean.js",
             Test262ExecutionMode.Interpreted);
 
+    [Fact]
+    public void Proxy_constructor_rejects_null_targets()
+        => AssertPass(
+            "built-ins/Proxy/create-target-not-object-throw-null.js",
+            Test262ExecutionMode.Interpreted);
+
     [Theory]
     [InlineData("built-ins/Object/defineProperty/15.2.3.6-4-623.js")]
     [InlineData("built-ins/Object/defineProperty/15.2.3.6-4-624.js")]
