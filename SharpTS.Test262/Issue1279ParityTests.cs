d260b17856d792401fae82bbeff5c675db8dc6d8
Nick Nassiri <nick206@gmail.com>
2026-08-09 04:20:18 -0700

    test(interp): cover missing proxy target descriptors

--- SharpTS.Test262/Issue1279ParityTests.cs
+++ SharpTS.Test262/Issue1279ParityTests.cs
@@ -1644,6 +1644,12 @@ public sealed class Issue1279ParityTests
             "built-ins/Proxy/getOwnPropertyDescriptor/result-is-undefined.js",
             Test262ExecutionMode.Interpreted);
 
+    [Fact]
+    public void Proxy_getOwnPropertyDescriptor_accepts_missing_target_properties()
+        => AssertPass(
+            "built-ins/Proxy/getOwnPropertyDescriptor/result-is-undefined-targetdesc-is-undefined.js",
+            Test262ExecutionMode.Interpreted);
+
     [Theory]
     [InlineData("built-ins/Object/defineProperty/15.2.3.6-4-623.js")]
     [InlineData("built-ins/Object/defineProperty/15.2.3.6-4-624.js")]
