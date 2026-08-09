a2d2568cc575b3951cdb11661f2854e942c2df8e
Nick Nassiri <nick206@gmail.com>
2026-08-09 04:54:23 -0700

    test(interp): reject string proxy prototypes

--- SharpTS.Test262/Issue1279ParityTests.cs
+++ SharpTS.Test262/Issue1279ParityTests.cs
@@ -1830,6 +1830,12 @@ public sealed class Issue1279ParityTests
             "built-ins/Proxy/getPrototypeOf/trap-result-neither-object-nor-null-throws-number.js",
             Test262ExecutionMode.Interpreted);
 
+    [Fact]
+    public void Proxy_getPrototypeOf_rejects_string_results()
+        => AssertPass(
+            "built-ins/Proxy/getPrototypeOf/trap-result-neither-object-nor-null-throws-string.js",
+            Test262ExecutionMode.Interpreted);
+
     [Theory]
     [InlineData("built-ins/Object/defineProperty/15.2.3.6-4-623.js")]
     [InlineData("built-ins/Object/defineProperty/15.2.3.6-4-624.js")]
