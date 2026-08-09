e2a53c05bdecc1d80fd7a2034a47b5836fcfe11a
Nick Nassiri <nick206@gmail.com>
2026-08-09 06:54:18 -0700

    test(interp): propagate proxy construct trap errors

--- SharpTS.Test262/Issue1279ParityTests.cs
+++ SharpTS.Test262/Issue1279ParityTests.cs
@@ -2616,6 +2616,12 @@ public sealed class Issue1279ParityTests
             "built-ins/Proxy/construct/null-handler.js",
             Test262ExecutionMode.Interpreted);
 
+    [Fact]
+    public void Proxy_construct_propagates_abrupt_trap_completion()
+        => AssertPass(
+            "built-ins/Proxy/construct/return-is-abrupt.js",
+            Test262ExecutionMode.Interpreted);
+
     [Theory]
     [InlineData("built-ins/Object/defineProperty/15.2.3.6-4-623.js")]
     [InlineData("built-ins/Object/defineProperty/15.2.3.6-4-624.js")]
