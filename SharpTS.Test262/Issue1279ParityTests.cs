6910d82124d802e611e47675d83e014fc4ab84d5
Nick Nassiri <nick206@gmail.com>
2026-08-09 05:34:00 -0700

    test(interp): reject hidden fixed target properties

--- SharpTS.Test262/Issue1279ParityTests.cs
+++ SharpTS.Test262/Issue1279ParityTests.cs
@@ -2118,6 +2118,12 @@ public sealed class Issue1279ParityTests
             "built-ins/Proxy/deleteProperty/return-is-abrupt.js",
             Test262ExecutionMode.Interpreted);
 
+    [Fact]
+    public void Proxy_deleteProperty_rejects_hidden_fixed_target_properties()
+        => AssertPass(
+            "built-ins/Proxy/deleteProperty/targetdesc-is-configurable-target-is-not-extensible.js",
+            Test262ExecutionMode.Interpreted);
+
     [Theory]
     [InlineData("built-ins/Object/defineProperty/15.2.3.6-4-623.js")]
     [InlineData("built-ins/Object/defineProperty/15.2.3.6-4-624.js")]
