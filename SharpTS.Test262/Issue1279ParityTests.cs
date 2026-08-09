be1296004b35cf7e0d005ab924d37f9bda40dc6f
Nick Nassiri <nick206@gmail.com>
2026-08-09 07:07:58 -0700

    test(interp): keep Proxy revokers extensible

--- SharpTS.Test262/Issue1279ParityTests.cs
+++ SharpTS.Test262/Issue1279ParityTests.cs
@@ -2718,6 +2718,12 @@ public sealed class Issue1279ParityTests
             "built-ins/Proxy/revocable/proxy.js",
             Test262ExecutionMode.Interpreted);
 
+    [Fact]
+    public void Proxy_revocation_function_is_extensible()
+        => AssertPass(
+            "built-ins/Proxy/revocable/revocation-function-extensible.js",
+            Test262ExecutionMode.Interpreted);
+
     [Theory]
     [InlineData("built-ins/Object/defineProperty/15.2.3.6-4-623.js")]
     [InlineData("built-ins/Object/defineProperty/15.2.3.6-4-624.js")]
