9f281f15c05c3292596cbfc0e4a8b913fd5704cf
Nick Nassiri <nick206@gmail.com>
2026-08-09 07:22:53 -0700

    test(interp): reject undefined Proxy handlers

--- SharpTS.Test262/Issue1279ParityTests.cs
+++ SharpTS.Test262/Issue1279ParityTests.cs
@@ -2820,6 +2820,12 @@ public sealed class Issue1279ParityTests
             "built-ins/Proxy/create-handler-not-object-throw-symbol.js",
             Test262ExecutionMode.Interpreted);
 
+    [Fact]
+    public void Proxy_constructor_rejects_undefined_handlers()
+        => AssertPass(
+            "built-ins/Proxy/create-handler-not-object-throw-undefined.js",
+            Test262ExecutionMode.Interpreted);
+
     [Theory]
     [InlineData("built-ins/Object/defineProperty/15.2.3.6-4-623.js")]
     [InlineData("built-ins/Object/defineProperty/15.2.3.6-4-624.js")]
