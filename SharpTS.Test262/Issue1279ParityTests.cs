d721d7f2246c895fa5941a87ebd7ca1f172cea26
Nick Nassiri <nick206@gmail.com>
2026-08-09 04:49:15 -0700

    test(interp): reject revoked proxy prototype queries

--- SharpTS.Test262/Issue1279ParityTests.cs
+++ SharpTS.Test262/Issue1279ParityTests.cs
@@ -1776,6 +1776,12 @@ public sealed class Issue1279ParityTests
             "built-ins/Proxy/getPrototypeOf/not-extensible-same-proto.js",
             Test262ExecutionMode.Interpreted);
 
+    [Fact]
+    public void Proxy_getPrototypeOf_rejects_revoked_proxies()
+        => AssertPass(
+            "built-ins/Proxy/getPrototypeOf/null-handler.js",
+            Test262ExecutionMode.Interpreted);
+
     [Theory]
     [InlineData("built-ins/Object/defineProperty/15.2.3.6-4-623.js")]
     [InlineData("built-ins/Object/defineProperty/15.2.3.6-4-624.js")]
