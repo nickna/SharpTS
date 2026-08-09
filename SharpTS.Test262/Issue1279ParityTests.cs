6e27fbc72483ce58b4893a1e6d4c4f90a23b898c
Nick Nassiri <nick206@gmail.com>
2026-08-09 05:51:57 -0700

    test(interp): accept true proxy has results

--- SharpTS.Test262/Issue1279ParityTests.cs
+++ SharpTS.Test262/Issue1279ParityTests.cs
@@ -2220,6 +2220,12 @@ public sealed class Issue1279ParityTests
             "built-ins/Proxy/has/return-is-abrupt-in.js",
             Test262ExecutionMode.Interpreted);
 
+    [Fact]
+    public void Proxy_has_accepts_true_results_for_existing_properties()
+        => AssertPass(
+            "built-ins/Proxy/has/return-true-target-prop-exists.js",
+            Test262ExecutionMode.Interpreted);
+
     [Theory]
     [InlineData("built-ins/Object/defineProperty/15.2.3.6-4-623.js")]
     [InlineData("built-ins/Object/defineProperty/15.2.3.6-4-624.js")]
