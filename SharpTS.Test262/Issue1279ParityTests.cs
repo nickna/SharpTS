a1fde7a0d286e094208b0c91c8103d254bc05966
Nick Nassiri <nick206@gmail.com>
2026-08-09 07:24:54 -0700

    test(interp): reject number Proxy targets

--- SharpTS.Test262/Issue1279ParityTests.cs
+++ SharpTS.Test262/Issue1279ParityTests.cs
@@ -2838,6 +2838,12 @@ public sealed class Issue1279ParityTests
             "built-ins/Proxy/create-target-not-object-throw-null.js",
             Test262ExecutionMode.Interpreted);
 
+    [Fact]
+    public void Proxy_constructor_rejects_number_targets()
+        => AssertPass(
+            "built-ins/Proxy/create-target-not-object-throw-number.js",
+            Test262ExecutionMode.Interpreted);
+
     [Theory]
     [InlineData("built-ins/Object/defineProperty/15.2.3.6-4-623.js")]
     [InlineData("built-ins/Object/defineProperty/15.2.3.6-4-624.js")]
