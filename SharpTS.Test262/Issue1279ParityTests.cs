7caa19d6efd9a7b3009828f17c63c1058dc134e6
Nick Nassiri <nick206@gmail.com>
2026-08-09 06:39:34 -0700

    test(interp): forward ordinary undefined set traps

--- SharpTS.Test262/Issue1279ParityTests.cs
+++ SharpTS.Test262/Issue1279ParityTests.cs
@@ -2520,6 +2520,12 @@ public sealed class Issue1279ParityTests
             "built-ins/Proxy/set/trap-is-undefined-target-is-proxy.js",
             Test262ExecutionMode.Interpreted);
 
+    [Fact]
+    public void Proxy_set_forwards_undefined_traps()
+        => AssertPass(
+            "built-ins/Proxy/set/trap-is-undefined.js",
+            Test262ExecutionMode.Interpreted);
+
     [Theory]
     [InlineData("built-ins/Object/defineProperty/15.2.3.6-4-623.js")]
     [InlineData("built-ins/Object/defineProperty/15.2.3.6-4-624.js")]
