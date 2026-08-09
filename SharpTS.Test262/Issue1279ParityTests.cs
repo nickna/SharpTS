75371f0f7ffadb46f6464dcf29e77ca171c33b57
Nick Nassiri <nick206@gmail.com>
2026-08-09 06:51:29 -0700

    test(interp): cover proxy construct trap parameters

--- SharpTS.Test262/Issue1279ParityTests.cs
+++ SharpTS.Test262/Issue1279ParityTests.cs
@@ -2598,6 +2598,12 @@ public sealed class Issue1279ParityTests
             "built-ins/Proxy/construct/call-result.js",
             Test262ExecutionMode.Interpreted);
 
+    [Fact]
+    public void Proxy_construct_passes_spec_trap_arguments()
+        => AssertPass(
+            "built-ins/Proxy/construct/call-parameters.js",
+            Test262ExecutionMode.Interpreted);
+
     [Theory]
     [InlineData("built-ins/Object/defineProperty/15.2.3.6-4-623.js")]
     [InlineData("built-ins/Object/defineProperty/15.2.3.6-4-624.js")]
