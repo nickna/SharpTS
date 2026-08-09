da063a41ebb33dd77331b31188624406869f7e30
Nick Nassiri <nick206@gmail.com>
2026-08-09 05:12:26 -0700

    test(interp): accept accurate extension prevention

--- SharpTS.Test262/Issue1279ParityTests.cs
+++ SharpTS.Test262/Issue1279ParityTests.cs
@@ -1944,6 +1944,12 @@ public sealed class Issue1279ParityTests
             "built-ins/Proxy/preventExtensions/return-true-target-is-extensible.js",
             Test262ExecutionMode.Interpreted);
 
+    [Fact]
+    public void Proxy_preventExtensions_accepts_nonextensible_targets()
+        => AssertPass(
+            "built-ins/Proxy/preventExtensions/return-true-target-is-not-extensible.js",
+            Test262ExecutionMode.Interpreted);
+
     [Theory]
     [InlineData("built-ins/Object/defineProperty/15.2.3.6-4-623.js")]
     [InlineData("built-ins/Object/defineProperty/15.2.3.6-4-624.js")]
