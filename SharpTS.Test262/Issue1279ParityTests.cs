04a26134275b8d1eaf77f82d48ca4cd5993bc3e7
Nick Nassiri <nick206@gmail.com>
2026-08-09 07:16:48 -0700

    test(interp): preserve revoked callable Proxy targets

--- SharpTS.Test262/Issue1279ParityTests.cs
+++ SharpTS.Test262/Issue1279ParityTests.cs
@@ -2778,6 +2778,12 @@ public sealed class Issue1279ParityTests
             "built-ins/Proxy/revocable/target-is-revoked-proxy.js",
             Test262ExecutionMode.Interpreted);
 
+    [Fact]
+    public void Proxy_revocable_preserves_revoked_callable_targets()
+        => AssertPass(
+            "built-ins/Proxy/revocable/target-is-revoked-function-proxy.js",
+            Test262ExecutionMode.Interpreted);
+
     [Theory]
     [InlineData("built-ins/Object/defineProperty/15.2.3.6-4-623.js")]
     [InlineData("built-ins/Object/defineProperty/15.2.3.6-4-624.js")]
