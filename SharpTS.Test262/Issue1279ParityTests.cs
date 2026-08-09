0e5353ec66fabf62ceb18795a7b7554dfb469cd4
Nick Nassiri <nick206@gmail.com>
2026-08-09 05:07:06 -0700

    test(interp): reject revoked extension prevention

--- SharpTS.Test262/Issue1279ParityTests.cs
+++ SharpTS.Test262/Issue1279ParityTests.cs
@@ -1920,6 +1920,12 @@ public sealed class Issue1279ParityTests
             "built-ins/Proxy/preventExtensions/call-parameters.js",
             Test262ExecutionMode.Interpreted);
 
+    [Fact]
+    public void Proxy_preventExtensions_rejects_revoked_proxies()
+        => AssertPass(
+            "built-ins/Proxy/preventExtensions/null-handler.js",
+            Test262ExecutionMode.Interpreted);
+
     [Theory]
     [InlineData("built-ins/Object/defineProperty/15.2.3.6-4-623.js")]
     [InlineData("built-ins/Object/defineProperty/15.2.3.6-4-624.js")]
