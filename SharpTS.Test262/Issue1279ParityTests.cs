4ad1bb7c48eadb504b7c5df280007f6a5e0f18dc
Nick Nassiri <nick206@gmail.com>
2026-08-09 04:01:49 -0700

    test(interp): reject boolean proxy ownKeys entries

--- SharpTS.Test262/Issue1279ParityTests.cs
+++ SharpTS.Test262/Issue1279ParityTests.cs
@@ -1575,6 +1575,7 @@ public sealed class Issue1279ParityTests
 
     [Theory]
     [InlineData("built-ins/Proxy/ownKeys/return-type-throws-array.js")]
+    [InlineData("built-ins/Proxy/ownKeys/return-type-throws-boolean.js")]
     public void Proxy_ownKeys_rejects_non_property_key_entries(string relativePath)
         => AssertPass(relativePath, Test262ExecutionMode.Interpreted);
 
