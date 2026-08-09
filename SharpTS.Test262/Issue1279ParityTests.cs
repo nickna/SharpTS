da30bb737ac29dabb1b574f6c785ef50cc5c9e7d
Nick Nassiri <nick206@gmail.com>
2026-08-09 05:01:13 -0700

    test(interp): cover nested extensibility forwarding

--- SharpTS.Test262/Issue1279ParityTests.cs
+++ SharpTS.Test262/Issue1279ParityTests.cs
@@ -1884,6 +1884,12 @@ public sealed class Issue1279ParityTests
             "built-ins/Proxy/isExtensible/return-same-result-from-target.js",
             Test262ExecutionMode.Interpreted);
 
+    [Fact]
+    public void Proxy_isExtensible_forwards_missing_traps_to_proxy_targets()
+        => AssertPass(
+            "built-ins/Proxy/isExtensible/trap-is-missing-target-is-proxy.js",
+            Test262ExecutionMode.Interpreted);
+
     [Theory]
     [InlineData("built-ins/Object/defineProperty/15.2.3.6-4-623.js")]
     [InlineData("built-ins/Object/defineProperty/15.2.3.6-4-624.js")]
