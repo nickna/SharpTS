74fdf98dab58eb5dbba93e3f9dbe924bcee683be
Nick Nassiri <nick206@gmail.com>
2026-08-09 07:10:28 -0700

    test(interp): reject constructing Proxy revokers

--- SharpTS.Test262/Issue1279ParityTests.cs
+++ SharpTS.Test262/Issue1279ParityTests.cs
@@ -2736,6 +2736,12 @@ public sealed class Issue1279ParityTests
             "built-ins/Proxy/revocable/revocation-function-name.js",
             Test262ExecutionMode.Interpreted);
 
+    [Fact]
+    public void Proxy_revocation_function_is_not_a_constructor()
+        => AssertPass(
+            "built-ins/Proxy/revocable/revocation-function-not-a-constructor.js",
+            Test262ExecutionMode.Interpreted);
+
     [Theory]
     [InlineData("built-ins/Object/defineProperty/15.2.3.6-4-623.js")]
     [InlineData("built-ins/Object/defineProperty/15.2.3.6-4-624.js")]
