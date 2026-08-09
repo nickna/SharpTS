c05cff43e5d0666232d6d1e2d04847d5270725df
Nick Nassiri <nick206@gmail.com>
2026-08-09 07:02:47 -0700

    test(interp): forward undefined proxy construct traps

--- SharpTS.Test262/Issue1279ParityTests.cs
+++ SharpTS.Test262/Issue1279ParityTests.cs
@@ -2676,6 +2676,12 @@ public sealed class Issue1279ParityTests
             "built-ins/Proxy/construct/trap-is-undefined-no-property.js",
             Test262ExecutionMode.Interpreted);
 
+    [Fact]
+    public void Proxy_construct_forwards_undefined_traps()
+        => AssertPass(
+            "built-ins/Proxy/construct/trap-is-undefined.js",
+            Test262ExecutionMode.Interpreted);
+
     [Theory]
     [InlineData("built-ins/Object/defineProperty/15.2.3.6-4-623.js")]
     [InlineData("built-ins/Object/defineProperty/15.2.3.6-4-624.js")]
