847955313e6dd8aa2f2a6c6d7864664513b36f83
Nick Nassiri <nick206@gmail.com>
2026-08-09 05:11:47 -0700

    test(interp): reject extensible proxy target lies

--- SharpTS.Test262/Issue1279ParityTests.cs
+++ SharpTS.Test262/Issue1279ParityTests.cs
@@ -1938,6 +1938,12 @@ public sealed class Issue1279ParityTests
             "built-ins/Proxy/preventExtensions/return-is-abrupt.js",
             Test262ExecutionMode.Interpreted);
 
+    [Fact]
+    public void Proxy_preventExtensions_rejects_extensible_target_lies()
+        => AssertPass(
+            "built-ins/Proxy/preventExtensions/return-true-target-is-extensible.js",
+            Test262ExecutionMode.Interpreted);
+
     [Theory]
     [InlineData("built-ins/Object/defineProperty/15.2.3.6-4-623.js")]
     [InlineData("built-ins/Object/defineProperty/15.2.3.6-4-624.js")]
