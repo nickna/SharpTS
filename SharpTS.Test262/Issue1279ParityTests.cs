638fb322d5a68dd61ccdfeca12d92feef5562310
Nick Nassiri <nick206@gmail.com>
2026-08-09 07:23:38 -0700

    test(interp): reject boolean Proxy targets

--- SharpTS.Test262/Issue1279ParityTests.cs
+++ SharpTS.Test262/Issue1279ParityTests.cs
@@ -2826,6 +2826,12 @@ public sealed class Issue1279ParityTests
             "built-ins/Proxy/create-handler-not-object-throw-undefined.js",
             Test262ExecutionMode.Interpreted);
 
+    [Fact]
+    public void Proxy_constructor_rejects_boolean_targets()
+        => AssertPass(
+            "built-ins/Proxy/create-target-not-object-throw-boolean.js",
+            Test262ExecutionMode.Interpreted);
+
     [Theory]
     [InlineData("built-ins/Object/defineProperty/15.2.3.6-4-623.js")]
     [InlineData("built-ins/Object/defineProperty/15.2.3.6-4-624.js")]
