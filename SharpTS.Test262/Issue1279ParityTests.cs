88b24ce337739b1627d57c2c378e09d8a656db32
Nick Nassiri <nick206@gmail.com>
2026-08-09 05:24:41 -0700

    test(interp): accept truthy prototype trap results

--- SharpTS.Test262/Issue1279ParityTests.cs
+++ SharpTS.Test262/Issue1279ParityTests.cs
@@ -2034,6 +2034,12 @@ public sealed class Issue1279ParityTests
             "built-ins/Proxy/setPrototypeOf/toboolean-trap-result-false.js",
             Test262ExecutionMode.Interpreted);
 
+    [Fact]
+    public void Proxy_setPrototypeOf_coerces_truthy_results_for_extensible_targets()
+        => AssertPass(
+            "built-ins/Proxy/setPrototypeOf/toboolean-trap-result-true-target-is-extensible.js",
+            Test262ExecutionMode.Interpreted);
+
     [Theory]
     [InlineData("built-ins/Object/defineProperty/15.2.3.6-4-623.js")]
     [InlineData("built-ins/Object/defineProperty/15.2.3.6-4-624.js")]
