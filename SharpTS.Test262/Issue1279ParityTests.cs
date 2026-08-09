5200b24c84e9ead65615cbdc9307ca411de505db
Nick Nassiri <nick206@gmail.com>
2026-08-09 06:11:53 -0700

    test(interp): forward null nested proxy get traps

--- SharpTS.Test262/Issue1279ParityTests.cs
+++ SharpTS.Test262/Issue1279ParityTests.cs
@@ -2340,6 +2340,12 @@ public sealed class Issue1279ParityTests
             "built-ins/Proxy/get/trap-is-not-callable.js",
             Test262ExecutionMode.Interpreted);
 
+    [Fact]
+    public void Proxy_get_forwards_null_traps_to_proxy_targets()
+        => AssertPass(
+            "built-ins/Proxy/get/trap-is-null-target-is-proxy.js",
+            Test262ExecutionMode.Interpreted);
+
     [Theory]
     [InlineData("built-ins/Object/defineProperty/15.2.3.6-4-623.js")]
     [InlineData("built-ins/Object/defineProperty/15.2.3.6-4-624.js")]
