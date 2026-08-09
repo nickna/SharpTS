04d2ea6f9823026a1b8b7cb763fe1d170149f4af
Nick Nassiri <nick206@gmail.com>
2026-08-09 05:22:03 -0700

    test(interp): propagate fixed prototype lookup errors

--- SharpTS.Test262/Issue1279ParityTests.cs
+++ SharpTS.Test262/Issue1279ParityTests.cs
@@ -2016,6 +2016,12 @@ public sealed class Issue1279ParityTests
             "built-ins/Proxy/setPrototypeOf/return-abrupt-from-isextensible-target.js",
             Test262ExecutionMode.Interpreted);
 
+    [Fact]
+    public void Proxy_setPrototypeOf_propagates_target_prototype_errors()
+        => AssertPass(
+            "built-ins/Proxy/setPrototypeOf/return-abrupt-from-target-getprototypeof.js",
+            Test262ExecutionMode.Interpreted);
+
     [Theory]
     [InlineData("built-ins/Object/defineProperty/15.2.3.6-4-623.js")]
     [InlineData("built-ins/Object/defineProperty/15.2.3.6-4-624.js")]
