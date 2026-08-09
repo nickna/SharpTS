9f02350506b233baf773b0b4c2813a95cb312796
Nick Nassiri <nick206@gmail.com>
2026-08-09 06:34:40 -0700

    test(interp): reject noncallable proxy set traps

--- SharpTS.Test262/Issue1279ParityTests.cs
+++ SharpTS.Test262/Issue1279ParityTests.cs
@@ -2490,6 +2490,12 @@ public sealed class Issue1279ParityTests
             "built-ins/Proxy/set/trap-is-missing-target-is-proxy.js",
             Test262ExecutionMode.Interpreted);
 
+    [Fact]
+    public void Proxy_set_rejects_noncallable_traps()
+        => AssertPass(
+            "built-ins/Proxy/set/trap-is-not-callable.js",
+            Test262ExecutionMode.Interpreted);
+
     [Theory]
     [InlineData("built-ins/Object/defineProperty/15.2.3.6-4-623.js")]
     [InlineData("built-ins/Object/defineProperty/15.2.3.6-4-624.js")]
