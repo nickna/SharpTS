3b511b7b6c6bf2ae2a5844be5591962d6ebf57a4
Nicholas Nassiri <nick@nassiri.xyz>
2026-09-19 01:08:41 -0700

    refactor: localize event emitter collection metadata

--- src/SharpTS/Compilation/EmittedEventEmitterRuntime.cs
+++ src/SharpTS/Compilation/EmittedEventEmitterRuntime.cs
@@ -198,10 +198,12 @@ public sealed class EmittedEventEmitterRuntime
     private static T Require<T>(T? handle, [CallerMemberName] string name = "") where T : class =>
         handle ?? throw new InvalidOperationException($"EventEmitter metadata '{name}' has not been declared.");
 
-    private void Set<T>(ref T? field, T value) where T : class
+    private void Set<T>(ref T? field, T value, [CallerMemberName] string name = "") where T : class
     {
         EnsureMutable();
         ArgumentNullException.ThrowIfNull(value);
+        if (field is not null)
+            throw new InvalidOperationException($"EventEmitter metadata '{name}' has already been declared.");
         field = value;
     }
 
